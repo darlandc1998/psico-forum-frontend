bfa13c4196c42b8e7ce7c2a840bc00493c7ae0a0
Darlan <darlan.candiotto98@gmail.com>
2022-06-24 20:00:57 -0300

    Implements post detail

--- src/pages/PublicPost/index.tsx
+++ src/pages/PublicPost/index.tsx
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import moment from 'moment';
 import api from '../../services';
-import { Container } from '../../components';
+import { Container, Loader } from '../../components';
 import Header from '../Header';
 import Content from '../Content';
 import Footer from '../Footer';
+import {
+  ContainerPost,
+  ContentPost,
+  TitlePost,
+  TextPost,
+  ContentDetailsPost,
+  CreationPost,
+  CreationTitlePost,
+  CreationValuePost,
+  AuthorPost,
+  AuthorTitlePost,
+  AuthorValuePost,
+} from './styles';
+
+interface UserRequest {
+  id: number;
+  name: string;
+}
+
+interface PostRequest {
+  id: number;
+  title: string;
+  text: string;
+  cover?: string;
+  published: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  author: UserRequest;
+}
 
 const PublicPost: React.FC = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
-  const [post, setPost] = useState();
+  const [post, setPost] = useState<PostRequest>();
 
   useEffect(() => {
     handleGetPublicPost();
@@ -18,8 +48,9 @@ const PublicPost: React.FC = () => {
   const handleGetPublicPost = async () => {
     setLoading(true);
     try {
-      const publicPost = await api.get(`/posts/${id}`);
+      const publicPost = await api.get<PostRequest>(`/posts/${id}`);
       setPost(publicPost.data);
+      console.log(publicPost.data);
     } catch (err) {
       console.error(err);
     } finally {
@@ -31,7 +62,28 @@ const PublicPost: React.FC = () => {
     <Container>
       <Header />
       <Content>
-        Detalhe do post: {loading} - {JSON.stringify(post)}
+        <ContainerPost>
+          {loading ? (
+            <Loader />
+          ) : (
+            <ContentPost>
+              <TitlePost>{post?.title}</TitlePost>
+              <ContentDetailsPost>
+                <AuthorPost>
+                  <AuthorTitlePost>Autor:</AuthorTitlePost>
+                  <AuthorValuePost>{post?.author?.name}</AuthorValuePost>
+                </AuthorPost>
+                <CreationPost>
+                  <CreationTitlePost>Criado em:</CreationTitlePost>
+                  <CreationValuePost>
+                    {moment(post?.createdAt).format('DD/MM/YYYY HH:mm:ss')}
+                  </CreationValuePost>
+                </CreationPost>
+              </ContentDetailsPost>
+              <TextPost>{post?.text}</TextPost>
+            </ContentPost>
+          )}
+        </ContainerPost>
       </Content>
       <Footer />
     </Container>
---
+++ src/pages/PublicPost/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { Content } from '../../components';
+
+export const ContainerPost = styled(Content)`
+  padding: 30px;
+`;
+
+export const ContentPost = styled(Content)`
+  flex: 1;
+`;
+
+export const TitlePost = styled(Content)`
+  font-size: 25px;
+  padding: 5px;
+`;
+
+export const TextPost = styled(Content)`
+  font-size: 15px;
+  padding: 5px;
+`;
+
+export const ContentDetailsPost = styled(Content)`
+  display: -webkit-box;
+`;
+
+export const AuthorPost = styled(Content)`
+  display: flex;
+  font-size: 13px;
+`;
+
+export const AuthorTitlePost = styled(Content)`
+  flex: initial;
+  margin-right: 5px;
+  font-weight: bold;
+`;
+
+export const AuthorValuePost = styled(Content)`
+  flex: initial;
+`;
+
+export const CreationPost = styled(Content)`
+  display: flex;
+  justify-content: end;
+  font-size: 13px;
+`;
+
+export const CreationTitlePost = styled(Content)`
+  flex: initial;
+  margin-right: 5px;
+  font-weight: bold;
+`;
+
+export const CreationValuePost = styled(Content)`
+  flex: initial;
+`;
